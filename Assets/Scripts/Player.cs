519882f6036b7eaa0a663884238c4f2e0ffbe6fb
connormullin <cmullin@ingenio.ca>
2022-10-22 22:50:17 -0700

    add player bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    private float horizontalBounds = 11.3f;
+    [SerializeField]
+    private float verticalBoundsUp = -2;
+    [SerializeField]
+    private float verticalBoundsDown = -4.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +22,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * _speed * Time.deltaTime);
+
+        if(transform.position.x > horizontalBounds || transform.position.x < -horizontalBounds)
+        {
+            transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+        }
+        if(transform.position.y >= verticalBoundsUp)
+        {
+            transform.position = new Vector3(transform.position.x, verticalBoundsUp, transform.position.z);
+        }
+        if (transform.position.y <= verticalBoundsDown)
+        {
+            transform.position = new Vector3(transform.position.x, verticalBoundsDown, transform.position.z);
+        }
     }
 }
