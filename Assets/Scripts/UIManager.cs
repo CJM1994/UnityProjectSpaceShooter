20c096d9790ea785686cc050930dc0a6f8afe60c
connormullin <connorjohnmullin@gmail.com>
2022-10-26 20:05:45 -0700

    add flickering game over text

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,6 +7,8 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private Text _scoreText;
+    [SerializeField]
+    private GameObject _gameOverText;
     private Image _livesImage;
     [SerializeField]
     private Sprite[] _livesSprites;
@@ -16,12 +18,13 @@ public class UIManager : MonoBehaviour
     {
         _scoreText.text = "Score: 0";
         _livesImage = GameObject.Find("Lives Image").transform.GetComponent<Image>();
+        _gameOverText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void UpdateScore(int playerScore)
@@ -31,23 +34,19 @@ public class UIManager : MonoBehaviour
 
     public void UpdateLives(int lives)
     {
-        switch (lives)
+        _livesImage.sprite = _livesSprites[lives];
+        if (lives == 0)
         {
-            case 3:
-                _livesImage.sprite = _livesSprites[0];
-                break;
-            case 2:
-                _livesImage.sprite = _livesSprites[1];
-                break;
-            case 1:
-                _livesImage.sprite = _livesSprites[2];
-                break;
-            case 0:
-                _livesImage.sprite = _livesSprites[3];
-                break;
-            default:
-                _livesImage.sprite = _livesSprites[0];
-                break;
+            StartCoroutine(flashGameOverText(lives));
+        }
+    }
+
+    IEnumerator flashGameOverText(int lives)
+    {
+        while (lives == 0)
+        {
+            _gameOverText.SetActive(!_gameOverText.activeSelf);
+            yield return new WaitForSeconds(0.5f);
         }
     }
 }
