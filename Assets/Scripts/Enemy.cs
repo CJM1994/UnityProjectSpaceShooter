1fed58ff0848ef6d7edfae6dfe868cb93a416ff0
connormullin <connorjohnmullin@gmail.com>
2022-10-30 13:02:23 -0700

    make enemy explosion separate object and add thrusters to player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,13 +7,13 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private float _speed = 4f;
     private Player _player;
-    private Animator _animator;
     private BoxCollider2D _boxCollider2D;
+    [SerializeField]
+    private GameObject _explosion;
 
     private void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        _animator = gameObject.GetComponent<Animator>();
         _boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
     }
 
@@ -44,9 +44,8 @@ public class Enemy : MonoBehaviour
 
     void handleEnemyDeath()
     {
-        Destroy(this.gameObject, 2.8f);
-        _speed = 0;
-        _animator.SetTrigger("EnemyDeath");
-        Destroy(_boxCollider2D);
+        GameObject explosionAnimation = Instantiate(_explosion, transform.position, Quaternion.identity);
+        Destroy(explosionAnimation, 2.8f);
+        Destroy(this.gameObject);
     }
 }
